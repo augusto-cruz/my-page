d298625df1fb03163eafd8cb86b6c1ed6773c2b5
Augusto Cruz <augustocruz@softbox.com.br>
2020-03-12 08:26:57 -0300

    Updates

---
+++ precache-manifest.4f6f05f7e419df0de9b55a38e785c38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f7290611350eca2237890f357c8354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b1bc82cbdaad00f9c2",
+    "url": "/static/css/main.101a8d44.chunk.css"
+  },
+  {
+    "revision": "b31e9b5abaff34bc17d0",
+    "url": "/static/js/2.298a5a37.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.298a5a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b1bc82cbdaad00f9c2",
+    "url": "/static/js/main.5b0b5f2b.chunk.js"
+  },
+  {
+    "revision": "32598ec3594c9db0baf6",
+    "url": "/static/js/runtime-main.048911ff.js"
+  },
+  {
+    "revision": "18d67a820f21ea335737f22960aa88b9",
+    "url": "/static/media/background.18d67a82.jpg"
+  }
+]);
